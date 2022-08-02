14a324fa058933bfacd40d4dd72784275b25e162
Ninad Ambade <ninad.ambade@aploris.com>
2022-08-02 19:58:16 +0530

    create react app

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          This line was added ninad
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
